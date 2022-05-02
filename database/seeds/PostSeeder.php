8d60191dc36cf6cbbdb9e80c768d6d210ba77e7e
AlfaProgrammer <alexloghin@outlook.it>
2022-05-02 20:03:31 +0200

    Popolato Tabella Posts

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +11,17 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 100 ; $i++) { 
+            $post = new Post();
+
+            $post->title = $faker->words(7, true);
+            $post->slug = Str::slug( $post->title );
+            $post->content = $faker->paragraphs(10, true);
+            $post->published_at = $faker->randomElement([null, $faker->dateTime()]);
+
+            $post->save();
+        }
     }
 }
