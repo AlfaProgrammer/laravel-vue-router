d56cc343472d2311a3f14abaead90000a936fb8e
AlfaProgrammer <alexloghin@outlook.it>
2022-05-03 17:55:56 +0200

    Sistemato form in create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -7,29 +7,31 @@
 
         <form action="{{ route('admin.posts.store') }}" method="POST">
             @csrf
-            <div>
-                <input type="text" name="title" id="post-title" placeholder="Inserisci il titolo del Post" value="{{old('title')}}">
+            <div class="mb-3">
+                <label for="post-title" class="form-label">Titolo Post</label>
+                <input class="form-control" type="text" name="title" id="post-title" placeholder="Inserisci il titolo del Post" value="{{old('title')}}">
                 @error('title')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
 
-            <div>
-                <textarea name="content" id="post-content" cols="100" rows="10" placeholder="Inserisci il contenuto del Post">
+            <div class="mb-3">
+                <label for="post-content" class="form-label">Contenuto del post</label>
+                <textarea class="form-control" name="content" id="post-content" cols="100" rows="10" placeholder="Inserisci il contenuto del Post">
                     {{old('content')}}
                 </textarea>
             </div>
 
-            <div>
-                <input type="text" name="cover" id="post-cover" placeholder="url immagine" value="{{old('cover')}}">
+            <div class="mb-3">
+                <label for="post-cover" class="form-label">Contenuto del post</label>
+                <input class="form-control" type="text" name="cover" id="post-cover" placeholder="url immagine" value="{{old('cover')}}">
             </div>
 
-           <div>
+           <div class="mb-3">
                 <button type="submit" class="btn btn-primary"> Post </button>
            </div>
 
         </form>
-
-        
+                
     </div>
 @endsection
